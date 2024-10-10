89aa6a64584dd2fc44c7be10d71ed1488c2b4af6
Machadoerica <144906356+Machadoerica@users.noreply.github.com>
2024-10-10 10:19:46 -0300

    ok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,7 +3,10 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function visualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    console.log(dados);
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container_texto')
+    paragrafo.innerHTML = `Vocẽ sabia que o mundo tem${dados.total_pessoas_mundo} de pessoas e que aproximaamente 
+    ${dados.total_passoas_conectas} estão conectadas em aguma rede social e pasam em média ${dados.tempo_medio} horas conectadas.`
 }
 
 visualizarInformacoesGlobais()
