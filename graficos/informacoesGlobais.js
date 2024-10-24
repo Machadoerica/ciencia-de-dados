f2d08347766ad874b03816aad2643286b2f64329
Machadoerica <144906356+Machadoerica@users.noreply.github.com>
2024-10-24 10:36:16 -0300

    ok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -6,8 +6,8 @@ async function visualizarInformacoesGlobais() {
     const pessoasConectadas = (dados.total_pessoas_conectadas) / 1e9
     const pessoasNoMundo = (dados.total_pessoas_mundo) / 1e9
     const horas = parseInt (dados.tempo_medio)
-    const minutos = math.round ((dados.tempo_medio-horas) * 100)
-    const porcentagemConectada = ((pessoasConetadas/pessoasNoMundo) * 100) .toFixed (2)
+    const minutos = math.round ((dados.tempo_medio - horas) * 100)
+    const porcentagemConectada = ((pessoasConectadas/pessoasNoMundo) * 100) .toFixed (2)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container_texto')
     paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${pessoasNoMundo} bilhões </span> de pessoas e que aproximadamente 
